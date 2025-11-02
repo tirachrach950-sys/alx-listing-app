fe9cd5d4aa7776ab98ca05281c740213687b4bdc
tirachrach950-sys <tirachrach950@gmail.com>
2025-11-02 19:34:07 +0100

    Add Button component with props and default values

--- Button.tsx
+++ Button.tsx
@@ -1,1 +1,18 @@
+import React from 'react'
+
+interface ButtonProps {
+  label: string
+  onClick?: () => void
+  type?: 'button' | 'submit' | 'reset'
+  variant?: 'primary' | 'secondary'
+}
+
+const Button: React.FC<ButtonProps> = ({
+  label,
+  onClick,
+  type = 'button',
+  variant = 'primary',
+}) => {
+  const baseStyle =
+    'px-6 py-2 rounde
 
